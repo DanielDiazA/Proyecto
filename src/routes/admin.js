89c98a1978509c98e636758603292151ad32453c
abc <abc@example.com>
2020-04-19 17:27:31 +0200

    landing page

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -8,15 +8,21 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 const Order = require('../models/order');
 
-//Usuarios//
+////////////////////////Usuarios/////////////////////////////////
 //all users//
 router.get("/users", isAuthenticated, async (req, res) => {
   const users = await User.find();
   res.render("admin/all-users", { users });
 });
 
+//Elimianar usuario//
+router.delete("/users/delete/:id", isAuthenticated, async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  req.flash("success_msg", "Perfil de usuario eliminado correctamente");
+  res.redirect("/users");
+});
 
-//Pedidos//
+/////////////////////////////Pedidos///////////////////////////////////
 //all orders//
 router.get("/orders", isAuthenticated, async (req, res) => {
     const orders = await Order.find().sort({ date: "desc" });
@@ -25,7 +31,19 @@ router.get("/orders", isAuthenticated, async (req, res) => {
 
 
 
-//Productos//
+//////////////////////////////Productos//////////////////////////////////
+//All products//
+router.get("/products", isAuthenticated, async (req, res) => {
+  const products = await Product.find();
+  res.render("admin/all-products", { products });
+});
+//Eliminar producto//
+router.delete("/products/delete/:id", isAuthenticated, async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  req.flash("success_msg", "Producto eliminado correctamente");
+  res.redirect("/products");
+});
+
 
 
 module.exports = router;
